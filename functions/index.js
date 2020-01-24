751b446efbea0c6a175a646f8e4689c6c49db831
0BugFound <alessio.tart@gmail.com>
2020-01-24 14:07:23 +0100

    test visual code

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
